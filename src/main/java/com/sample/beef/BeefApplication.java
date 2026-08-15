283d2fc1b6ee916a3f9c96abc52df62fc645e9ac
rabikax13 <rabikax13@gmail.com>
2026-08-15 14:39:32 +0530

    name changed

--- src/main/java/com/sample/beef/BeefApplication.java
+++ src/main/java/com/sample/beef/BeefApplication.java
@@ -8,7 +8,7 @@ public class BeefApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeefApplication.class, args);
-		System.out.println("Ajay");
+		System.out.println("Rabika");
 	}
 
 }
