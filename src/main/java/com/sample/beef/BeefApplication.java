786654141591060232c2cabed17c018507d19724
rabikax13 <rabikax13@gmail.com>
2026-08-15 14:26:52 +0530

    intial commit

---
+++ src/main/java/com/sample/beef/BeefApplication.java
@@ -0,0 +1,13 @@
+package com.sample.beef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/beef/BeefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.beef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
